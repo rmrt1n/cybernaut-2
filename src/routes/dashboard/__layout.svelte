a6f0e43db6b5bb05a6823bef4efe4be45febc84b
Ryan Martin <51780559+rmrt1n@users.noreply.github.com>
2022-02-14 15:45:12 +0800

    paused unauthenticated redirect for demo

--- src/routes/dashboard/__layout.svelte
+++ src/routes/dashboard/__layout.svelte
@@ -1,7 +1,8 @@
 <script context="module">
-  export const load = async ({ session }) => {
-    const { authenticated } = session;
-    return !authenticated ? { status: 302, redirect: '/signin' } : {};
+  export const load = async (/* { session } */) => {
+    /* const { authenticated } = session; */
+    /* return !authenticated ? { status: 302, redirect: '/signin' } : {}; */
+    return {};
   };
 </script>
 
